4c9cbaf7799d1306f60c2a937586738940ec3561
Jonah <jonahm@bu.edu>
2023-09-14 02:12:43 -0400

    Added the rest of the numbers

--- app/src/main/java/com/example/calcr_v2/MainActivity.kt
+++ app/src/main/java/com/example/calcr_v2/MainActivity.kt
@@ -9,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var button_1: Button
     private lateinit var button_2: Button
     private lateinit var button_3: Button
+    private lateinit var button_4: Button
+    private lateinit var button_5: Button
+    private lateinit var button_6: Button
+    private lateinit var button_7: Button
+    private lateinit var button_8: Button
+    private lateinit var button_9: Button
     private lateinit var button_plus: Button
     private lateinit var button_minus: Button
     private lateinit var button_times: Button
@@ -34,6 +40,13 @@ class MainActivity : AppCompatActivity() {
         button_1 = findViewById(R.id.button_1)
         button_2 = findViewById(R.id.button_2)
         button_3 = findViewById(R.id.button_3)
+        button_4 = findViewById(R.id.button_4)
+        button_5 = findViewById(R.id.button_5)
+        button_6 = findViewById(R.id.button_6)
+        button_7 = findViewById(R.id.button_7)
+        button_8 = findViewById(R.id.button_8)
+        button_9 = findViewById(R.id.button_9)
+
         button_plus = findViewById(R.id.button_plus)
         button_minus = findViewById(R.id.button_minus)
         button_times = findViewById(R.id.button_times)
@@ -75,6 +88,30 @@ class MainActivity : AppCompatActivity() {
 
         button_3.setOnClickListener{
             load(3)
+        }
+
+        button_4.setOnClickListener{
+            load(4)
+        }
+
+        button_5.setOnClickListener{
+            load(5)
+        }
+
+        button_6.setOnClickListener{
+            load(6)
+        }
+
+        button_7.setOnClickListener{
+            load(7)
+        }
+
+        button_8.setOnClickListener{
+            load(8)
+        }
+
+        button_9.setOnClickListener{
+            load(9)
         }
 
         button_plus.setOnClickListener{
